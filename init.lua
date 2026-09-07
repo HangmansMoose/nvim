8e9da98c0246eccded11b94f045d84b83c6b6287
HangmansMoose <trivial86@protonmail.com>
2026-09-07 14:46:29 +1000

    which key popup now bottom right ('helix' mode)

--- init.lua
+++ init.lua
@@ -338,8 +338,15 @@ do
   -- Useful plugin to show you pending keybinds.
   vim.pack.add { gh 'folke/which-key.nvim' }
   require('which-key').setup {
+  -- have which-key popup in bottom right corner
+    preset = "helix", -- nothing to do with keybinds 
+    --win = {
+    --  col = -1,
+    --  row = -1,
+    --  anchor = "SE",
+    --},
     -- Delay between pressing a key and opening which-key (milliseconds)
-    delay = 0,
+    delay = 500,
     icons = { mappings = vim.g.have_nerd_font },
     -- Document existing key chains
     spec = {
@@ -349,30 +356,6 @@ do
       { 'gr', group = 'LSP Actions', mode = { 'n' } },
     },
   }
-
-  -- [[ Colorscheme ]]
-  -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command under that to load whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --vim.pack.add { gh 'folke/tokyonight.nvim' }
-  -----@diagnostic disable-next-line: missing-fields
-  --require('tokyonight').setup {
-  --  styles = {
-  --    comments = { italic = false }, -- Disable italics in comments
-  --  },
-  --}
-
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  -- vim.cmd.colorscheme 'tokyonight-night'
-
-
-  -- Highlight todo, notes, etc in comments
-  --vim.pack.add { gh 'folke/todo-comments.nvim' }
-  --require('todo-comments').setup { signs = false }
 
   -- [[ mini.nvim ]]
   --  A collection of various small independent plugins/modules
